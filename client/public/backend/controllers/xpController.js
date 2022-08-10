510aced7e6cb346c6002d2a4af94935e37207c6f
Michael Hancock <shanehancock64@live.com>
2022-08-09 21:02:07 -0700

    Error Handler

--- client/public/backend/controllers/xpController.js
+++ client/public/backend/controllers/xpController.js
@@ -9,7 +9,14 @@ const getXp = (req, res) => {
 // @route POST /api/xp
 // @access authentication will be required
 const setXp = (req, res) => {
-  res.status(200).json({message: 'Post XP'})
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+   
+
+  }
+  res.status(200).json({message: 'Set Xp'})
+  
 };
 
 // PUT Xp 
--- client/public/backend/server.js
+++ client/public/backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port  = process.env.PORT || 5000;
+const {errorHandler} = require('./middleware/errorMiddleware');
 
-const app = express();
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: false}))
+
+
+
 
 app.use('/api/xp', require('./routes/xpRoutes'))
+
+app.use(errorHandler)
 
 
 app.listen(port, () => console.log(`Server started on ${port}`));
