45491ee3db50607e8607fccdecb35b992a37b699
Michael Hancock <shanehancock64@live.com>
2022-08-09 21:22:33 -0700

    Update xpController.js
    1

--- client/public/backend/controllers/xpController.js
+++ client/public/backend/controllers/xpController.js
@@ -1,16 +1,14 @@
-const asyncHandler = require('express-async-handler');
-
 // Get Xp 
 // @route GET /api/xp
 // @access authentication will be required
-const getXp = asyncHandler(async (req, res) => {
+const getXp = (req, res) => {
   res.status(200).json({message: 'Get XP'})
-});
+};
 
 // POST Xp 
 // @route POST /api/xp
 // @access authentication will be required
-
+const setXp = (req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
@@ -18,21 +16,22 @@ const getXp = asyncHandler(async (req, res) => {
 
   }
   res.status(200).json({message: 'Set Xp'})
-
+  
+};
 
 // PUT Xp 
 // @route PUT /api/xp/:id
 // @access authentication will be required
-const updateXp = asyncHandler(async  (req, res) => {
+const updateXp = (req, res) => {
   res.status(200).json({message: `Update XP ${req.params.id}`})
-});
+};
 
 // DELETE Xp 
 // @route DELETE /api/xp/:id
 // @access authentication will be required
-const deleteXp = asyncHandler(async (req, res) => {
+const deleteXp = (req, res) => {
   res.status(200).json({message: `Delete XP ${req.params.id}`}) 
-});
+};
 
 module.exports = {
   getXp,
