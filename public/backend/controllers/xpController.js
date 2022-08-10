eb3c37a2f57770185d6e1461d31fdb1dd73f3ac6
Michael Hancock <shanehancock64@live.com>
2022-08-09 19:02:38 -0700

    Controllers

--- public/backend/controllers/xpController.js
+++ public/backend/controllers/xpController.js
@@ -1,7 +1,34 @@
+// Get Xp 
+// @route GET /api/xp
+// @access authentication will be required
 const getXp = (req, res) => {
   res.status(200).json({message: 'Get XP'})
 };
 
+// POST Xp 
+// @route POST /api/xp
+// @access authentication will be required
+const setXp = (req, res) => {
+  res.status(200).json({message: 'Post XP'})
+};
+
+// PUT Xp 
+// @route PUT /api/xp/:id
+// @access authentication will be required
+const updateXp = (req, res) => {
+  res.status(200).json({message: `Update XP ${req.params.id}`})
+};
+
+// DELETE Xp 
+// @route DELETE /api/xp/:id
+// @access authentication will be required
+const deleteXp = (req, res) => {
+  res.status(200).json({message: `Delete XP ${req.params.id}`}) 
+};
+
 module.exports = {
   getXp,
+  setXp,
+  updateXp,
+  deleteXp,
 };
--- public/backend/routes/xpRoutes.js
+++ public/backend/routes/xpRoutes.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router()
-const {getXp} = require('../controllers/xpController');
+const {getXp, setXp, updateXp, deleteXp,} = require('../controllers/xpController');
 
 router.get('/', getXp);
 
+router.post('/', setXp);
 
-router.post('/', (req, res) => {
-  res.status(200).json({message: 'Post XP'})
-});
+router.put('/:id', updateXp);
 
-router.put('/:id', (req, res) => {
-  res.status(200).json({message: `Update XP ${req.params.id}`})
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({message: `Delete XP ${req.params.id}`})
-});
+router.delete('/:id', deleteXp);
 
 
 
