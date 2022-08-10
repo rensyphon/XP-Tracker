ff20433388bd40348dbb3101659c812c813b9c9b
Michael Hancock <shanehancock64@live.com>
2022-08-09 18:23:33 -0700

    Routes

--- public/backend/server.js
+++ public/backend/server.js
@@ -5,3 +5,7 @@ const port  = process.env.PORT || 5000;
 const app = express();
 
 app.listen(port, () => console.log(`Server started on ${port}`));
+
+app.get('/api/xp', (req, res) => {
+  res.status(200).json({message: 'Get XP'})
+});
